930ae34149481a04c247c3adab1d43121dbe2f5a
eth0net <eth0net@pm.me>
2026-04-01 00:48:36 +0100

    add contact page

--- src/routes/contact/contact.tsx
+++ src/routes/contact/contact.tsx
@@ -1,7 +1,19 @@
+import type { CarouselImage } from "../../components/carousel";
+import "./contact.css";
+
+const images: CarouselImage[] = [
+  { alt: "Black and white cat", url: "/cat_neeks.jpg" },
+];
+
 function Contact() {
   return (
     <div id="contact">
+      <img src={images[0].url} alt={images[0].alt} />
       <h1>Contact</h1>
+      <p>Holly Rebecca Thomas</p>
+      <a href="mailto:holly@hollyrebeccaartwork.co.uk">
+        holly@hollyrebeccaartwork.co.uk
+      </a>
     </div>
   );
 }
